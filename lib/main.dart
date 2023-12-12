4ceb261ee4f18b96a52e4df89b058154f3545c5f
iilic89 <i.ilic@p4it.com>
2023-12-12 08:39:34 +0100

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -45,40 +45,16 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       appBar: AppBar(
-       
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
       ),
-      body:  Stack(
+      body: const Stack(
         children: [
            Center(
-            child: Stack(
-              children: [
-                Positioned(
-                  right: 1,
-                  top: 1,
-                  child: Container(
-              width: 100,
-              height: 100,
-              color: Colors.red,
-            ),
-            ),
-              ],
-              )
-       
-
+            child: Text("First change"),
       )
         ],
-
       )
-      
-      
-     
-      
-      
-      
-
     );
   }
 }
